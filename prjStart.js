26db21e4e6c03d82d40224ae821cf0e7d52b7bc8
Jacob Jones <jacobfilmjones@hotmail.com>
2017-06-07 15:17:14 +0100

    spec file + spec folder creation

--- prjStart.js
+++ prjStart.js
@@ -1,10 +1,16 @@
 const fs = require('fs');
-const path = '/home/jacob/Desktop/code/new_project'
+const path = '/home/jacob/Desktop/code/new_project';
+const specPath = '/home/jacob/Desktop/code/new_project/spec';
 fs.mkdir(path, (err) =>{
-    if(err === null) makeFile(path + '/main.js');
-    else console.log('Error: File already exists');
+    if(err === null) {
+        fs.mkdir(specPath, (err) => {
+        if(err === null) {
+            fs.writeFile(path + '/main.js');
+            fs.writeFile(path + '/.gitignore');
+            fs.writeFile(path + '/spec/main.spec.js');
+        }
+        else throw new Error('Error: File already exists');
+      });
+    }
+    else throw new Error('Error: File already exists');
 });
-
-function makeFile (name, cb) {
-    fs.writeFile(name, cb)
-}
