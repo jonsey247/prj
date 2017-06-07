22ef1bc8ae178b408dac2de6df06e82e4a6b15d6
Jacob Jones <jacobfilmjones@hotmail.com>
2017-06-07 16:44:23 +0100

    create new project with variable name

--- prjStart.js
+++ prjStart.js
@@ -1,17 +1,17 @@
 const fs = require('fs');
 const execute = require('child_process').exec;
-const adie = '/Users/adrianwilliams/Documents/northcoders/accelerate/week_5/newProject';
-// const jacob = '~/code/new_project';
-const specPath = adie + '/spec';
-
-fs.mkdir(adie, (err) => {
+// const Jacob = '/Users/adrianwilliams/Documents/northcoders/accelerate/week_5/newProject';
+const projName = process.argv[2];
+const Jacob = '/home/jacob/Desktop/code/' + projName;
+const specPath = Jacob + '/spec';
+fs.mkdir(Jacob, (err) => {
     if (err === null) {
         fs.mkdir(specPath, (err) => {
         if (err === null) {
-            fs.writeFile(adie + '/main.js');
-            fs.writeFile(adie + '/.gitignore');
-            fs.writeFile(adie + '/spec/main.spec.js');
-            execute(`cd ${adie} && npm init -y && git init && npm i -D Chai && npm i -D eslint && npm i -D husky`, (err) => {
+            fs.writeFile(Jacob + '/main.js');
+            fs.writeFile(Jacob + '/.gitignore');
+            fs.writeFile(Jacob + '/spec/main.spec.js');
+            execute(`cd ${Jacob} && npm init -y && git init && npm i -D chai && npm i -D eslint && npm i -D husky`, (err) => {
                 console.log(err);
             });
         }
